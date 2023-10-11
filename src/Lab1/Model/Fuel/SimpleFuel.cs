7ff9fb1d3f0f691495e18e9a417650d03c22e4af
lipipidron <lociX55lim@gmail.com>
2023-10-11 20:08:23 +0300

    refactor: now have auto-property Amount

--- src/Lab1/Model/Fuel/SimpleFuel.cs
+++ src/Lab1/Model/Fuel/SimpleFuel.cs
@@ -1,5 +1,11 @@
 namespace Itmo.ObjectOrientedProgramming.Lab1.Model.Fuel;
 
-public class SimpleFuel : Fuel
+public class SimpleFuel : IFuel
 {
+    public double Amount { get; private set; }
+
+    public void Use(double time)
+    {
+        Amount += time;
+    }
 }
