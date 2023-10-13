2864026515e1ac16cecdf645dc1fffec584122c7
lipipidron <lociX55lim@gmail.com>
2023-10-13 21:06:56 +0300

    refactor: changed time formula

--- src/Lab1/Entities/Engine/EngineE.cs
+++ src/Lab1/Entities/Engine/EngineE.cs
@@ -4,17 +4,14 @@ namespace Itmo.ObjectOrientedProgramming.Lab1.Entities.Engine;
 
 public class EngineE : IEngine
 {
-    private const double Speed = 40;
     private const int StartCost = 50;
     public SimpleFuel Fuel { get; } = new();
     public double Time { get; private set; }
 
     public void Move(int distance)
     {
-        double speed = Speed;
-        speed += double.Exp(distance);
-        double time = distance / speed;
+        double time = double.Log(distance + 1);
         Time += time;
-        Fuel.Use((time * 1.5) + StartCost);
+        Fuel.Use(time + StartCost);
     }
 }
