bead8f2eb8da5670bfc7c871924486a27a8b5a58
lipipidron <lociX55lim@gmail.com>
2023-10-14 23:08:36 +0300

    refactor: changes related to general changes refactor: now accepts Collection

--- src/Lab1/Service/Travel/TravelWay.cs
+++ src/Lab1/Service/Travel/TravelWay.cs
@@ -1,7 +1,5 @@
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections.ObjectModel;
 using Itmo.ObjectOrientedProgramming.Lab1.Entities.Environment;
-using Itmo.ObjectOrientedProgramming.Lab1.Entities.Obstacle;
 using Itmo.ObjectOrientedProgramming.Lab1.Entities.Ship;
 using Itmo.ObjectOrientedProgramming.Lab1.Model.Fuel;
 using Itmo.ObjectOrientedProgramming.Lab1.Model.Result;
@@ -10,44 +8,28 @@ namespace Itmo.ObjectOrientedProgramming.Lab1.Service.Travel;
 
 public class TravelWay
 {
-    private readonly List<IEnvironment> _environments;
+    private readonly ReadOnlyCollection<IEnvironment> _environments;
     private readonly FuelExchange.FuelExchange _fuelExchange;
 
-    public TravelWay(FuelExchange.FuelExchange fuelExchange)
+    public TravelWay(FuelExchange.FuelExchange fuelExchange, ReadOnlyCollection<IEnvironment> environments)
     {
-        _environments = new List<IEnvironment>();
+        _environments = environments;
         _fuelExchange = fuelExchange;
-    }
-
-    public void AddEnvironment(IEnvironment environment)
-    {
-        _environments.Add(environment);
     }
 
     public Result Travel(Ship ship)
     {
+        Result success = new Result.Success(new Collection<IFuel>(), new Collection<double>());
         foreach (IEnvironment environment in _environments)
         {
-            if (environment.TryMove(ship) is false)
-            {
-                return new Result.LostShip();
-            }
+            Result result = environment.TryOvercome(ship);
 
-            IEnumerable<IObstacle> obstacle = environment.GetAllObstacles();
-            foreach (IObstacle obs in obstacle)
-            {
-                Result res = obs.DoDamage(ship);
-                if (res is not Result.ObstacleReflected)
-                {
-                    return res;
-                }
-            }
+            if (result is not Result.SuccessEnvironment successEnvironment ||
+                success is not Result.Success resultSuccess) return result;
+            resultSuccess.Fuel.Add(successEnvironment.Fuel);
+            resultSuccess.Time.Add(successEnvironment.Time);
         }
 
-        IEnumerable<IFuel> allFuel = ship.FuelSpend();
-        double cost = allFuel.Sum(fuel => _fuelExchange.TotalCost(fuel));
-        double time = ship.GetAllTime();
-
-        return new Result.Success(cost, time);
+        return success;
     }
 }
