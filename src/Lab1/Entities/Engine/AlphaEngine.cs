fa1111ce0feb9db0e9791371e5ece68a8ccc6598
lipipidron <lociX55lim@gmail.com>
2023-10-14 22:58:48 +0300

    refactor: realise func from interface

--- src/Lab1/Entities/Engine/AlphaEngine.cs
+++ src/Lab1/Entities/Engine/AlphaEngine.cs
@@ -6,7 +6,6 @@ namespace Itmo.ObjectOrientedProgramming.Lab1.Entities.Engine;
 public class AlphaEngine : IJumpEngine
 {
     public double JumpDistance { get; init; } = 10;
-    public SpecialFuel Fuel { get; init; } = new();
     public double Time { get; private set; }
 
     public JumpEngineTravelResult Travel(int distance)
@@ -17,7 +16,7 @@ public class AlphaEngine : IJumpEngine
         }
 
         Time = distance / JumpDistance;
-        Fuel.Use(Time);
-        return new JumpEngineTravelResult.TravelSuccess(Time, Fuel);
+        var fuel = new SpecialFuel(Time);
+        return new JumpEngineTravelResult.TravelSuccess(Time, fuel);
     }
 }
