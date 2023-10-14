2c1d9014fa205bd2a5d2efdb28d7f245f5c67d82
lipipidron <lociX55lim@gmail.com>
2023-10-14 23:04:29 +0300

    refactor: realise func from interface

--- src/Lab1/Entities/Engine/OmegaEngine.cs
+++ src/Lab1/Entities/Engine/OmegaEngine.cs
@@ -6,7 +6,6 @@ namespace Itmo.ObjectOrientedProgramming.Lab1.Entities.Engine;
 public class OmegaEngine : IJumpEngine
 {
     public double JumpDistance { get; init; } = 20;
-    public SpecialFuel Fuel { get; init; } = new();
     public double Time { get; private set; }
 
     public JumpEngineTravelResult Travel(int distance)
@@ -17,7 +16,7 @@ public class OmegaEngine : IJumpEngine
         }
 
         Time = distance / JumpDistance;
-        Fuel.Use(Time);
-        return new JumpEngineTravelResult.TravelSuccess(Time, Fuel);
+        var fuel = new SpecialFuel(Time);
+        return new JumpEngineTravelResult.TravelSuccess(Time, fuel);
     }
 }
