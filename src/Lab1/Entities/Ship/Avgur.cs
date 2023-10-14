8c5e8cba061b5574a65e1f528f2f39bc1ae1cf6e
lipipidron <lociX55lim@gmail.com>
2023-10-14 14:56:02 +0300

    feat: add logic

--- src/Lab1/Entities/Ship/Avgur.cs
+++ src/Lab1/Entities/Ship/Avgur.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Itmo.ObjectOrientedProgramming.Lab1.Entities.Deflector;
 using Itmo.ObjectOrientedProgramming.Lab1.Entities.Engine;
 using Itmo.ObjectOrientedProgramming.Lab1.Entities.Stability;
+using Itmo.ObjectOrientedProgramming.Lab1.Model.Fuel;
 
 namespace Itmo.ObjectOrientedProgramming.Lab1.Entities.Ship;
 
@@ -12,6 +15,23 @@ public class Avgur : Ship
         JumpEngine = new AlphaEngine();
         JumpDistance = JumpEngine.JumpDistance;
         Deflector = new Deflector3();
-        Stability = new Stability3();
+        Stability = new Hull3();
+    }
+
+    public override IEnumerable<IFuel> FuelSpend()
+    {
+        var allFuel = new Collection<IFuel>();
+        if (Engine is not null) allFuel.Add(Engine.Fuel);
+        if (JumpEngine is not null) allFuel.Add(JumpEngine.Fuel);
+
+        return allFuel;
+    }
+
+    public override double GetAllTime()
+    {
+        double fullTime = 0;
+        if (Engine is not null) fullTime += Engine.Time;
+        if (JumpEngine is not null) fullTime += JumpEngine.Time;
+        return fullTime;
     }
 }
