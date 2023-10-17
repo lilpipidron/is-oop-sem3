6be26e60122c9258ff932121ec9e2d1adf6bba4e
lipipidron <lociX55lim@gmail.com>
2023-10-17 12:35:09 +0300

    feat: add _connectionOption

--- src/Lab2/Entities/Ssd/Ssd.cs
+++ src/Lab2/Entities/Ssd/Ssd.cs
@@ -2,15 +2,16 @@ namespace Itmo.ObjectOrientedProgramming.Lab2.Entities.Ssd;
 
 public class Ssd : ISsd
 {
-    // пока хз, потом разберусь с подключением
     private readonly int _memoryAmount;
     private readonly int _maximumSpeed;
     private readonly int _powerConsumption;
+    private readonly string _connectionOption;
 
-    public Ssd(int memoryAmount, int maximumSpeed, int powerConsumption)
+    public Ssd(int memoryAmount, int maximumSpeed, int powerConsumption, string connectionOption)
     {
         _memoryAmount = memoryAmount;
         _maximumSpeed = maximumSpeed;
         _powerConsumption = powerConsumption;
+        _connectionOption = connectionOption;
     }
 }
